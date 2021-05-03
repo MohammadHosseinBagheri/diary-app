b8a8c95b1fdf1c9d3045a2e4131f120255856d74
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-05-04 03:42:02 +0430

    register

---
+++ src/screens/auth/register/RegisterScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const RegisterScreen = () => {
+    return (
+        <View>
+            <Text>Register</Text>
+        </View>
+    )
+}
+
+export default RegisterScreen
+
+const styles = StyleSheet.create({})
