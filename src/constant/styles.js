89ae426f0f9ec80345749eb92a71171e201ec168
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-05-05 02:15:24 +0430

    add danger color and success color

--- src/constant/styles.js
+++ src/constant/styles.js
@@ -3,4 +3,6 @@ import { PixelRatio } from "react-native";
 export const BACKGROUND_COLOR="#11233c";
 export const MAIN_PADDING=PixelRatio.get()*5;
 export const ICON_COLOR="#fff";
-export const HOME_FLATLIST_BACKGROUND="#1a345b"
+export const HOME_FLATLIST_BACKGROUND="#1a345b";
+export const DANGER_COLOR="tomato";
+export const SUCCESS_COLOR="#00E676"
