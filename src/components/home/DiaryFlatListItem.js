0f1e43f03a1e1e6c51319fa3b86412c21ae6c87f
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-05-06 02:27:56 +0430

    flat list item

---
+++ src/components/home/DiaryFlatListItem.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from 'react-native';
+import {
+  FONT_FAMILY,
+  HOME_FLATLIST_BACKGROUND,
+  MAIN_PADDING,
+} from '../../constant/styles';
+import * as Animatable from 'react-native-animatable';
+import CardNavbar from '../common/nav-bar/CardNavbar';
+
+const DiaryFlatListItem = props => {
+  const {title, text, id, index} = props;
+  const animation = {
+    0: {translateX: 80 * index, opacity: 0},
+    1: {translateX: 0, opacity: 1},
+  };
+  return (
+    <Animatable.View
+      animation={animation}
+      duration={700}
+      useNativeDriver
+      delay={index * 1000}
+      style={styles.container}>
+      <TouchableOpacity style={{flex: 1}}>
+        <View style={{flexDirection: 'row'}}>
+          <CardNavbar backgroundColor="red"/>
+          <CardNavbar backgroundColor="#FFFF00"/>
+          <CardNavbar backgroundColor="#00E676"/>
+        </View>
+        <View
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
+          <Text style={styles.titleText}>Title: {title}</Text>
+          <ImageBackground
+            style={{
+              width: 50,
+              height: 50,
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+            }}
+            source={require('../../assets/icons/Calendar.png')}>
+            <Text style={{marginBottom: 10, color: '#fff'}}>
+              {new Date().getDay()}
+            </Text>
+          </ImageBackground>
+        </View>
+        <Text style={styles.text}>{text}</Text>
+      </TouchableOpacity>
+    </Animatable.View>
+  );
+};
+
+export default DiaryFlatListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: HOME_FLATLIST_BACKGROUND,
+    minHeight: 150,
+    marginBottom: MAIN_PADDING,
+    marginTop: MAIN_PADDING,
+    borderRadius: 8,
+    // borderWidth: 1,
+    // borderColor: '#F2BE54',
+    padding: MAIN_PADDING,
+    elevation: 5,
+    zIndex: 1,
+  },
+  titleText: {
+    fontFamily: FONT_FAMILY,
+    fontSize: 20,
+    color: 'white',
+  },
+  text: {
+    fontFamily: FONT_FAMILY,
+    color: 'white',
+    fontSize: 16,
+  },
+});
