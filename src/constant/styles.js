64c1106eb99d995a5c8a8a75029f1fd7eecd0a14
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-05-06 02:28:27 +0430

    font family

--- src/constant/styles.js
+++ src/constant/styles.js
@@ -5,4 +5,5 @@ export const MAIN_PADDING=PixelRatio.get()*5;
 export const ICON_COLOR="#fff";
 export const HOME_FLATLIST_BACKGROUND="#1a345b";
 export const DANGER_COLOR="tomato";
-export const SUCCESS_COLOR="#00E676"
+export const SUCCESS_COLOR="#00E676";
+export const FONT_FAMILY='Itim-Regular'
