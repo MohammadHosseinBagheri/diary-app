842592b55b33e45626decadd78ea36a3d70036ec
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-05-07 16:59:53 +0430

    login functionality

--- src/utils/login/login.js
+++ src/utils/login/login.js
@@ -4,11 +4,9 @@ import {HOME_SCREEN} from '../../constant/routes';
 export const handleSubmit = async (values, navigation, realm, alertOpen) => {
   //get user
   let user = await realm.objects('user');
-
-  const isExist = await user.find(item => item.email === values.email);
-
+  // console.log(user.length)
   //if user not found create user
-  if (!isExist) {
+  if (user.length === 0) {
     await realm.write(async () => {
       //create user
       let res = realm.create('user', {
@@ -17,23 +15,29 @@ export const handleSubmit = async (values, navigation, realm, alertOpen) => {
         password: values.password,
       });
       if (res) {
-       await realm.close();
-       await navigation.navigate(HOME_SCREEN);
-      } else alertOpen('isnt', false);
-    });
-  }
-  //user found and now check validation
-  else {
-    const {email, password} = isExist;
-    //check password
-    if (password === values.password) {
-      alertOpen('Successful', true);
-      setTimeout(async () => {
         await realm.close();
         await navigation.navigate(HOME_SCREEN);
-      }, 2000);
-    } else {
-      alertOpen('Check Username or Password');
+      } else alertOpen('isnt', false);
+    });
+  } else if (user.length !== 0) {
+    const isExist = await user.find(item => item.email === values.email);
+    if (!isExist) alertOpen('Username or password are invalid', false);
+    else {
+      const isExist = await user.find(item => item.email === values.email);
+      console.log(isExist);
+
+      const {email, password} = isExist;
+      //check password
+      if (password === values.password) {
+        alertOpen('Successful', true);
+        setTimeout(async () => {
+          await realm.close();
+          console.log(navigation)
+          await navigation.navigate(HOME_SCREEN);
+        }, 2000);
+      } else {
+        alertOpen('Check Username or Password');
+      }
     }
   }
 };
